3968da4ce856b1eea9e2e6be943b4578078a333b
Witchayut Jongpattanasombut <skpepe134@gmail.com>
2020-09-18 10:55:38 +0700

    show is ssr

--- pages/[ssr]/index.js
+++ pages/[ssr]/index.js
@@ -1,7 +1,16 @@
 import { unstable_renderSubtreeIntoContainer } from "react-dom";
 import { useRouter } from "next/router";
 
-export default () => {
+export default ({ isSsr }) => {
   const router = useRouter();
-  return <div>{router.query.ssr}</div>;
+  return (
+    <div>
+      <p>{isSsr ? "SSR" : "NOT SSR"}</p>
+      <p>{router.query.ssr}</p>
+    </div>
+  );
+};
+
+export const getServerSideProps = () => {
+  return { isSsr: true };
 };
